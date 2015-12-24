98ce7aab8a24d26dbecda9e8ab335fb370b22822
makafis <vinnyalfieri@gmail.com>
2015-12-24 10:56:48 -0500

    created schema and working on test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+  artists = Artist.create([
+  { name: 'Pink Floyd'},
+  { name: ''},
+  { name: ''},
+  { name: ''},
+  { name: ''},
+  { name: ''},
+  { name: ''},
+  { name: ''},
+  { name: ''}
+  ])
