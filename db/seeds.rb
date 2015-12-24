4315a547e61f3d53788f1b8c1029856c415750c6
makafis <vinnyalfieri@gmail.com>
2015-12-24 11:09:06 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,40 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+  Artist.destroy_all
+  Song.destroy_all
+  Album.destroy_all
+  
+  @pinkfloyd = Artist.create({ name: 'Pink Floyd'})
+  @zeppelin = Artist.create({ name: 'Led Zeppelin'})
+  @rage = Artist.create({ name: 'Rage Against the Machine'})
+  @queen = Artist.create({ name: 'Queen'})
+  @blink = Artist.create({ name: 'Blink 182'})
+  @van = Artist.create({ name: 'Van Morrison'})
+  @nirvana = Artist.create({ name: 'Nirvana'})
+  @strokes = Artist.create({ name: 'The Strokes'})
+  @metallica = Artist.create({ name: 'Metallica'})
+  
 
-  artists = Artist.create([
-  { name: 'Pink Floyd'},
-  { name: ''},
-  { name: ''},
-  { name: ''},
-  { name: ''},
-  { name: ''},
-  { name: ''},
-  { name: ''},
-  { name: ''}
-  ])
+  @pinkfloyd_album = Album.create({name: 'Pink Floyds Album'})
+  @pinkfloyd.albums << @pinkfloyd_album
+
+  @song1 = Song.create({title: 'Welcome to the Machine'})
+  @song1.artist = @pinkfloyd
+  @song1.album = @pinkfloyd_album
+  @song1.save
+  @song2 = Song.create({title: 'Wish You Were Here'})
+  @song2.artist = @pinkfloyd
+  @song2.album = @pinkfloyd_album
+  @song2.save
+  @song3 = Song.create({title: 'Dogs'})
+  @song3.artist = @pinkfloyd
+  @song3.album = @pinkfloyd_album
+  @song3.save
+  @song4 = Song.create({title: 'Vera'})
+  @song4.artist = @pinkfloyd
+  @song4.album = @pinkfloyd_album
+  @song4.save
+
+
+
