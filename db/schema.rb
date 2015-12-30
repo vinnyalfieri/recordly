2a4dd98f3ef98476a46c3bcf320c4290fce7bbb0
makafis <vinnyalfieri@gmail.com>
2015-12-30 11:54:26 -0500

    changed to postgres database for deployment

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151230050556) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "albums", force: :cascade do |t|
     t.string   "name"
     t.integer  "artist_id"
